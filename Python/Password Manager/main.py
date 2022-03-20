a589cb038358e9ebecec49f4b91d18b8e9b0af33
sign4git <47028894+sign4git@users.noreply.github.com>
2022-03-20 19:15:29 +0530

    Changed file format to json

--- Python/Password Manager/main.py
+++ Python/Password Manager/main.py
@@ -1,6 +1,7 @@
 from random import choice, randint, shuffle
 from tkinter import messagebox, Tk, Canvas, PhotoImage, Label, Entry, Button
 import pyperclip
+import json
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
@@ -32,19 +33,56 @@ def save():
     website = website_entry.get()
     email = email_entry.get()
     password = pwd_entry.get()
+    json_data = {
+        website: {
+            "email": email,
+            "password": password
+        }
+    }
     if len(website) > 0 and len(email) > 0:
-        pwd_details = f"{website} | {email} | {password}\n"
+        # pwd_details = f"{website} | {email} | {password}\n"
         user_consent = messagebox.askokcancel(title=website,
                                               message=f"Here are the details\n Email: {email}\n "
                                                       f"Password: {password}\n Do you want to save?")
         if user_consent:
-            website_entry.delete(0, 'end')
-            pwd_entry.delete(0, 'end')
-            with open("data.txt", mode='a') as file:
-                file.writelines(pwd_details)
-            messagebox.showinfo(title="Password Manager", message="Credentials Saved Successfully")
+            try:
+                # Load Data
+                with open("data.json", mode='r') as file:
+                    data = json.load(file)
+                    # Update Data
+                    data.update(json_data)
+            except FileNotFoundError:
+                # Create new file and save Data
+                with open("data.json", mode='w') as file:
+                    json.dump(json_data, file, indent=4)
+            else:
+                # Open and update Data
+                with open("data.json", mode='w') as file:
+                    json.dump(data, file, indent=4)
+                messagebox.showinfo(title="Password Manager", message="Credentials Saved Successfully")
+            finally:
+                website_entry.delete(0, 'end')
+                pwd_entry.delete(0, 'end')
     else:
         messagebox.showwarning(title="Error", message="Please enter all the details")
+
+
+# ---------------------------- FIND PASSWORD ------------------------------- #
+def find_password():
+    website = website_entry.get()
+    try:
+        with open("data.json", "r") as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        messagebox.showinfo(title=website, message="No Data file found")
+    else:
+        if website in data:
+            user_details = f"Email is : {data[website]['email']}\n Password is : {data[website]['password']}"
+            messagebox.showinfo(title=website, message=user_details)
+        else:
+            messagebox.showerror(title=website, message=f"Credentials for {website} does not exists")
+    finally:
+        website_entry.delete(0, "end")
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -64,7 +102,7 @@ email_label.grid(row=2, column=0)
 pwd_label = Label(text="Password:")
 pwd_label.grid(row=3, column=0)
 
-website_entry = Entry()
+website_entry = Entry(width=21)
 website_entry.grid(row=1, column=1, columnspan=2, sticky="EW")
 website_entry.focus()
 email_entry = Entry()
@@ -73,6 +111,8 @@ email_entry.insert(0, "test@mailinator.com")
 pwd_entry = Entry(width=21)
 pwd_entry.grid(row=3, column=1, sticky="EW")
 
+search_btn = Button(text="Search", command=find_password)
+search_btn.grid(row=1, column=2, sticky="EW")
 gen_pwd_btn = Button(text="Generate Password", command=generate_password)
 gen_pwd_btn.grid(row=3, column=2, sticky="EW")
 add_btn = Button(text="Add", width=36, command=save)
