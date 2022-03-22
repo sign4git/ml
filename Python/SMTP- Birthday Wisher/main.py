2872c22c4e6910de981be0ecab50495d6b0923c2
sign4git <47028894+sign4git@users.noreply.github.com>
2022-03-22 11:48:54 +0530

    Added SMTP Config to send email

--- Python/SMTP- Birthday Wisher/main.py
+++ Python/SMTP- Birthday Wisher/main.py
@@ -1,14 +1,32 @@
+import os
+import pandas as pd
+import datetime as dt
+import random
 import smtplib
 from decouple import config
 
 username = config('user', default='')
 password = config('password', default='')
-to_address = config('to_address', default='')
 
-with smtplib.SMTP("smtp.gmail.com") as connection:
-    connection.starttls()
-    connection.login(user=username, password=password)
-    connection.sendmail(from_addr=username, to_addrs=to_address, msg="Subject:This is my email from "
-                                                                     "SMTP \n\n I am writing this "
-                                                                     "email to test SMTP module from "
-                                                                     "Python")
+birthday_data = pd.read_csv("birthdays.csv")
+birthday_list = birthday_data.to_dict(orient="records")
+
+letter_templates = []
+for (dir_path, dir_names, filenames) in os.walk("letter_templates"):
+    letter_templates += [os.path.join(dir_path, file) for file in filenames]
+
+now = dt.datetime.now()
+today_day = now.day
+today_month = now.month
+
+for current_person in birthday_list:
+    if today_day == current_person["day"] and today_month == current_person["month"]:
+        letter = random.choice(letter_templates)
+        with open(letter, "r") as file:
+            data = file.read()
+            data = str(data).replace('[NAME]', current_person["name"])
+            with smtplib.SMTP("smtp.gmail.com") as connection:
+                connection.starttls()
+                connection.login(user=username, password=password)
+                connection.sendmail(from_addr=username, to_addrs=current_person['email'],
+                                    msg=f"Subject:Happy Birthday {current_person['name']}\n\n {data}")
