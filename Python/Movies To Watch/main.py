05cd5eb8b51d60622cffb1a5c0b9cd780cc41310
sign4git <47028894+sign4git@users.noreply.github.com>
2022-04-03 19:39:33 +0530

    List of movies to watch using data scraping

--- Python/Movies To Watch/main.py
+++ Python/Movies To Watch/main.py
@@ -1,14 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 
-response = requests.\
+response = requests. \
     get("https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/")
-soup = BeautifulSoup(response.text,"html.parser")
-movies_tag = soup.find_all(name="h3" ,class_="title")
+soup = BeautifulSoup(response.text, "html.parser")
+movies_tag = soup.find_all(name="h3", class_="title")
 movies_list = []
-with open("movies_to_watch.txt",mode="w") as file:
+with open("movies_to_watch.txt", mode="w") as file:
     for movies in movies_tag:
         movies_list.append(movies.getText())
-        file.writelines(movies.getText()+"\n")
-
-
+        file.writelines(movies.getText() + "\n")
